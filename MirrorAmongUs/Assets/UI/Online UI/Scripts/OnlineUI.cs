a5567345c6a04732976eafc22e4cb4b59a8eedcc
LeeHW <aadd306@naver.com>
2022-09-26 15:36:57 +0900

    온라인 UI 기능 , 방만들기 시스템 생성

---
+++ MirrorAmongUs/Assets/UI/Online UI/Scripts/OnlineUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnlineUI : MonoBehaviour
+{
+    [SerializeField]
+    InputField nicknameInputField;
+
+    [SerializeField]
+    GameObject createRoomUI;
+
+    public void OnClickCreateRoomButton()
+    {
+        if(nicknameInputField.text != "")
+        {
+            PlayerSettings.nickname = nicknameInputField.text;
+            createRoomUI.SetActive(true);
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            nicknameInputField.GetComponent<Animator>().SetTrigger("On");
+        }
+    }
+}
