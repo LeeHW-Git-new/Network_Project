bb973edaa17f529b2195fd93840df84b4ab92dff
LeeHW <aadd306@naver.com>
2022-09-28 19:48:15 +0900

    대기실 캐릭터 구현

---
+++ MirrorAmongUs/Assets/Character/Scripts/CharacterMover.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CharacterMover : NetworkBehaviour
+{
+    Animator animator;
+
+    public bool isMoveable;
+
+    [SyncVar]
+    public float speed = 2f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        if(hasAuthority)
+        {
+            Camera cam = Camera.main;
+            cam.transform.SetParent(transform);
+            cam.transform.localPosition = new Vector3(0f, 0f, -10f);
+            cam.orthographicSize = 2.5f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        if(hasAuthority && isMoveable)
+        {
+            bool isMove = false;
+
+            if(PlayerSettings.controlType == EControlType.KeyboardMouse)
+            {
+                Vector3 dir = Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f), 1f);
+                if (dir.x < 0f) transform.localScale = new Vector3(-0.5f, 0.5f, 1f);
+                else if (dir.x > 0f) transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+                transform.position += dir * speed * Time.deltaTime;
+                isMove = dir.magnitude != 0f;
+            }
+            else
+            {
+                if(Input.GetMouseButton(0))
+                {
+                    Vector3 dir = (Input.mousePosition - new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0f)).normalized;
+                    if (dir.x < 0f) transform.localScale = new Vector3(-0.5f, 0.5f, 1f);
+                    else if (dir.x > 0f) transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+                    transform.position += dir * speed * Time.deltaTime;
+                    isMove = dir.magnitude != 0f;
+                }
+            }
+            animator.SetBool("isMove", isMove);
+        }
+    }
+}
---
+++ MirrorAmongUs/Assets/Scripts/SortingSprite.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingSprite : MonoBehaviour
+{
+    public enum ESortingType
+    {
+        Static, Updata
+    }
+
+    [SerializeField]
+    ESortingType sortingType;
+
+    SpriteSorter sorter;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        sorter = FindObjectOfType<SpriteSorter>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        spriteRenderer.sortingOrder = sorter.GetSortingOrder(gameObject);
+    }
+
+    void Update()
+    {
+        if(sortingType == ESortingType.Updata)
+        {
+            spriteRenderer.sortingOrder = sorter.GetSortingOrder(gameObject);
+        }
+    }
+}
